7301114793474a7d2952c7d25642c9c4c7e26f13
Emanuele Carlini <emanuele.carlini@isti.cnr.it>
2014-10-09 11:30:46 +0200

    perliminary version of the latency contraint class

--- src/it/cnr/isti/smartfed/metascheduler/constraints/LatencyConstraint.java
+++ src/it/cnr/isti/smartfed/metascheduler/constraints/LatencyConstraint.java
@@ -1,11 +1,5 @@
 package it.cnr.isti.smartfed.metascheduler.constraints;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
-
-import it.cnr.isti.smartfed.federation.CostComputer;
 import it.cnr.isti.smartfed.federation.application.ApplicationEdge;
 import it.cnr.isti.smartfed.metascheduler.MSPolicy;
 import it.cnr.isti.smartfed.metascheduler.resources.MSApplication;
@@ -14,6 +8,8 @@ import it.cnr.isti.smartfed.metascheduler.resources.iface.IMSApplication;
 import it.cnr.isti.smartfed.metascheduler.resources.iface.IMSProvider;
 import it.cnr.isti.smartfed.networking.InternetEstimator;
 
+import java.util.Set;
+
 import org.jgap.Gene;
 import org.jgap.IChromosome;
 
@@ -21,7 +17,7 @@ public class LatencyConstraint extends MSPolicy
 {
 
 	public LatencyConstraint(double weight, char type) {
-		super(weight, type);
+		super(weight, MSPolicy.DESCENDENT_TYPE);
 	}
 
 	@Override
@@ -48,7 +44,13 @@ public class LatencyConstraint extends MSPolicy
 				int target_index = MSPolicy.getGeneIndexFromNodeId(e.getTargetVmId(), chromos.getGenes(), app);			
 				int targetProvider = (int) chromos.getGenes()[target_index].getAllele();
 				
-				// check this edge against the InternetEstimator
+				// check this edge's latency requirement against internet estimator
+				int internet_latency = internet.getInternetLink(prov.getID(), targetProvider).getLatency();
+				int application_latency = (int) e.getLatency();
+				
+				// TODO: evaluate the distance?
+				// calculateDistance_ErrHandling(internet_latency, application_latency, 10000); //TODO
+				// faccio la media
 			}
 		}
 

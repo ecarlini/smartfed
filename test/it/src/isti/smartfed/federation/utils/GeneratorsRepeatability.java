d76af01a3cd4e0a6f97ef39fd71c607a54268fda
Gaetano <g.anastasi@isti.cnr.it>
2014-10-20 16:21:56 +0200

    Restoring classpath

--- test/it/src/isti/smartfed/federation/utils/GeneratorsRepeatability.java
+++ test/it/src/isti/smartfed/federation/utils/GeneratorsRepeatability.java
@@ -1,22 +1,22 @@
 /*
 Copyright 2014 ISTI-CNR
- 
+
 This file is part of SmartFed.
 
 SmartFed is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
- 
+
 SmartFed is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
- 
+
 You should have received a copy of the GNU General Public License
 along with SmartFed. If not, see <http://www.gnu.org/licenses/>.
 
-*/
+ */
 
 package it.src.isti.smartfed.federation.utils;
 
@@ -29,6 +29,7 @@ import it.cnr.isti.smartfed.federation.utils.DatacenterGenerator;
 import java.util.Calendar;
 import java.util.List;
 
+import org.cloudbus.cloudsim.Host;
 import org.cloudbus.cloudsim.Log;
 import org.cloudbus.cloudsim.core.CloudSim;
 import org.junit.Assert;
@@ -42,32 +43,39 @@ public class GeneratorsRepeatability
 	{
 		Log.setDisabled(true);
 	}
-	
+
 	@Test
 	public void testDatacenterGenerator()
 	{
 		CloudSim.init(1, Calendar.getInstance(), true);
-		
+
 		DatacenterGenerator dg = new DatacenterGenerator(33);
 		List<FederationDatacenter> list = dg.getDatacenters(5, 10);
-		
+
 		StringBuilder sb1 = new StringBuilder();
 		for (FederationDatacenter fd: list)
-			sb1.append(fd.toString()).append("\n");
-		
+			sb1.append(fd.getDatacenterCharacteristicString()).append("\n");
+
 		ResourceCounter.reset();
 		CloudSim.init(1, Calendar.getInstance(), true);
-		
+
 		dg = new DatacenterGenerator(33);
 		list = dg.getDatacenters(5, 10);
-		
+
 		StringBuilder sb2 = new StringBuilder();
 		for (FederationDatacenter fd: list)
-			sb2.append(fd.toString()).append("\n");
-		
+			sb2.append(fd.getDatacenterCharacteristicString()).append("\n");
+
 		Assert.assertEquals(sb1.toString(), sb2.toString());
+
+		for (FederationDatacenter f : list){
+			List<Host> l = f.getHostList();
+			for (Host h : l){
+				Assert.assertEquals(h.getStorage(), f.getMSCharacteristics().getHighestStorage());
+			}
+		}
 	}
-	
+
 	@Test
 	public void testApplicationGenerator()
 	{		
@@ -78,7 +86,7 @@ public class GeneratorsRepeatability
 		ResourceCounter.reset();
 		ag = new ApplicationGenerator(43);
 		Application app2 = ag.getApplication(1, 2, 5);
-		
+
 		Assert.assertEquals(app1.allVMsString(), app2.allVMsString());
 	}
 }
